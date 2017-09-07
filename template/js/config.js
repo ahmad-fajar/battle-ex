2a21446e9489e4fb54ef2f6decfc447db6c2bbd2
Ahmad Fajar <shin.fajar@gmail.com>
2017-09-07 15:40:17 +0700

    template

---
+++ template/js/config.js
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2016 Google Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License")
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+/**
+ * This is just a place to put all your API authentication keys
+ */
+Config = function() {
+    return {
+        // You will need to replace with your own Firebase configuration
+        // This can be retrieved from clicking "Add App" from the project overview page.
+        firebase: {
+            apiKey: 'AIzaSyCl7oY_gdN1OLj8j2ZouMQIzCDH4vEsjKY',
+            authDomain: 'battle-ex.firebaseapp.com',
+            databaseURL: 'https://battle-ex.firebaseio.com',
+            storageBucket: 'battle-ex.appspot.com',
+            messagingSenderId: '979693056225'
+        },
+        // Replace with an API key for Google Cloud Vision API
+        // DO NOT DO THIS IN PRODUCTION!!! For Demo purposes only.
+        visionAPI: {
+            key: 'AIzaSyCgT4EmJ13yIX1Lwu-xPE4Yd_nnifEtAsc'
+        }
+    }
+}();
